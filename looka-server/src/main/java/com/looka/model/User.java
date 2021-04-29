7101e0a3ce4b0f09633565457f370a3ca430efdc
Emmy Steven <hello@emmysteven.com>
2021-04-29 17:13:54 +0100

    🎉 first commit

---
+++ looka-server/src/main/java/com/looka/model/User.java
@@ -0,0 +1,13 @@
+package com.looka.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.*;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown=true)
+public class User {
+
+    private String name;
+    private String blog;
+
+}
---
+++ looka-server/src/test/java/com/looka/LookaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.looka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LookaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
