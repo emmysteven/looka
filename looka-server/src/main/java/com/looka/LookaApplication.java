0c3fc3ff7e83c85fc36886bf95af460c2fec34c7
Emmy Steven <hello@emmysteven.com>
2021-04-29 17:48:42 +0100

    enable async in startup class

--- looka-server/src/main/java/com/looka/LookaApplication.java
+++ looka-server/src/main/java/com/looka/LookaApplication.java
@@ -2,12 +2,30 @@ package com.looka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
+import java.util.concurrent.Executor;
+
+@EnableAsync
 @SpringBootApplication
 public class LookaApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(LookaApplication.class, args);
+        // close the application context to shut down the custom ExecutorService
+        SpringApplication.run(LookaApplication.class, args).close();
+    }
+
+    @Bean
+    public Executor taskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(2);
+        executor.setMaxPoolSize(2);
+        executor.setQueueCapacity(500);
+        executor.setThreadNamePrefix("GithubLookup-");
+        executor.initialize();
+        return executor;
     }
 
 }
