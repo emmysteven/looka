23f38386e103df9f2f246b5f8bf0062489b528fb
Emmy Steven <hello@emmysteven.com>
2021-04-29 17:26:15 +0100

    docs: add JavaDoc comments

--- looka-server/src/main/java/com/looka/model/User.java
+++ looka-server/src/main/java/com/looka/model/User.java
@@ -3,6 +3,10 @@ package com.looka.model;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.*;
 
+/**
+ * @author Emmy Steven
+*/
+
 @Data
 @JsonIgnoreProperties(ignoreUnknown=true)
 public class User {
